7d55174822680cfdb200658eb4a42d58a42634a8
oussama-el1 <oussama284@gmail.com>
2024-04-25 01:34:01 +0100

    soleve task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,9 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => {
-      result.map((elt) => ({
+      const mappedResult = result.map((elt) => ({
         status: elt.status,
         value: Object.prototype.hasOwnProperty.call(elt, 'reason') ? elt.reason : elt.value,
       }));
+      return mappedResult;
     });
 }
