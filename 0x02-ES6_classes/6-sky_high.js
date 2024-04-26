fc93466800af30430194792f597c3da48e4e7438
oussama-el1 <oussama284@gmail.com>
2024-04-26 21:22:47 +0100

    ⚡

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,22 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  set floors(newfloors) {
+    if (typeof newfloors === 'number') {
+      this._floors = newfloors;
+    }
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
