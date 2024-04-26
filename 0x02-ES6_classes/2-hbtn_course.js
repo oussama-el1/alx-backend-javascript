ccc05a05bf0c314afe19441747b50001c131c858
oussama-el1 <oussama284@gmail.com>
2024-04-26 03:56:48 +0100

    😏 done

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,23 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new Error('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new Error('Length must be a number');
-    }
-    if (!Array.isArray(students)) {
-      throw new Error('Students must be an array');
-    }
-    for (const student of students) {
-      if (typeof student !== 'string') {
-        throw new Error('Each student must be a string');
-      }
-    }
-
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   set name(newName) {
