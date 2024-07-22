a5a7f4d2b1217347198a5368fc9ae9a06e2e542c
oussama-el1 <elhadraoui@284@gmail.com>
2024-07-22 19:09:42 +0100

    add file description

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,11 @@
-function displayMessage(str) {
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} str message to display in the STDOUT.
+ * @author oussama ELHADRAOUI <https://github.com/oussama-el1>
+ */
+
+const displayMessage = (str) => {
   process.stdout.write(str);
-}
+};
+
 module.exports = displayMessage;
