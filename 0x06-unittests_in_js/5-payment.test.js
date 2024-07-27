57d0d8c89e845a615735780398f5802173b6347d
oussama-el1 <elhadraoui@284@gmail.com>
2024-07-27 20:03:23 +0100

    .\5-payment.test.js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let spyLog;
+
+  beforeEach(() => {
+    if (!spyLog) {
+      spyLog = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    spyLog.log.resetHistory();
+  });
+
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spyLog.log.calledWith('The total is: 120')).to.be.true;
+    expect(spyLog.log.calledOnce).to.be.true;
+  });
+
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spyLog.log.calledWith('The total is: 20')).to.be.true;
+    expect(spyLog.log.calledOnce).to.be.true;
+  });
+});
